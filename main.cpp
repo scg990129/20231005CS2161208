4c3a2e2362d4b8448a298ebdb4a9d4b5f61e8301
Jacob Yeung <xohkgolden-git@yahoo.com>
2023-10-05 15:44:21 -0700

    first wrong version

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include "unorderedArrayListType.h"
+
+//Data:  18 42 78 22 42 5 42 57 
+using namespace std;
+int main(int argc, char* argv[]){
+        // Write your main here
+    return 0;
+}
